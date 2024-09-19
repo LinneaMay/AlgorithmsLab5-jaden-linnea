c9a1aaec7866fdc81a123d36abfde484c44193c5
LinneaMay <Linneamay@gilbertson.net>
2024-09-19 13:19:13 -0500

    added companypreerneces

--- lab5.java
+++ lab5.java
@@ -1,3 +1,14 @@
+import java.util.Collections;
+import java.util.List;
 public class lab5 {
+
+    String[][] companyPreferences = {{"2", "5", "1", "3", "4"}, 
+    {"1", "2", "3", "4", "5"},
+    {"5", "3", "2", "1", "4"},
+    {"1", "3", "2", "4", "5"},
+    {"2", "3", "5", "4", "1"}};
+
+    
+
     
 }
